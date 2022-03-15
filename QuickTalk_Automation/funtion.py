9c89f47876dde38c8ab0cf73ea74293fb23ac405
Piyush Wadhwa <97625078+piyushwadhwa7@users.noreply.github.com>
2022-03-15 15:12:17 +0530

    Update funtion.py

--- QuickTalk_Automation/funtion.py
+++ QuickTalk_Automation/funtion.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--useAutomationExtension=false")
 chrome_options.add_argument("--remote-debugging-port=9230")
