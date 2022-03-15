29337d3f1d6a309f23670a15c2ba9a7317f62fe6
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2022-03-15 14:41:36 +0530

    some changes was done - chrome options arguments were comment out and port one was deleted

--- QuickTalk_Automation/test_Click_onNewCreatedCard.py
+++ QuickTalk_Automation/test_Click_onNewCreatedCard.py
@@ -1,19 +1,19 @@
-import time
-
-import funtion
-import test_enter_name_new_qtalk
-from selenium.webdriver.common.by import By
-
-
-def test_click_on_new_created_card():
-    card_elements = funtion.driver.find_elements(By.CLASS_NAME, "card-main ")
-    print(card_elements)
-    for card_elem in card_elements:
-        title_elem = card_elem.find_element(By.CLASS_NAME, "talk--title")
-        if title_elem.text == "Automate quickTalk Created":
-            print(title_elem.text)
-            funtion.driver.execute_script("arguments[0].scrollIntoView()",card_elem)
-            print(card_elem)
-            time.sleep(3)
-            card_elem.click()
-            time.sleep(10)
+# import time
+# from selenium.webdriver.common.by import By
+#
+# from QuickTalk_Automation import funtion
+#
+# funtion.open_browser()
+#
+# def test_click_on_new_created_card():
+#     card_elements = funtion.driver.find_elements(By.CLASS_NAME, "card-main ")
+#     print(card_elements)
+#     for card_elem in card_elements:
+#         title_elem = card_elem.find_element(By.CLASS_NAME, "talk--title")
+#         if title_elem.text == "Automate quickTalk Created":
+#             print(title_elem.text)
+#             funtion.driver.execute_script("arguments[0].scrollIntoView()",card_elem)
+#             print(card_elem)
+#             time.sleep(3)
+#             card_elem.click()
+#             time.sleep(10)
